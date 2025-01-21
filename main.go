b7dfbc824cc1a213d3837ad63929fd098f05de6b
dakabirov <cloudlypower@gmail.com>
2025-01-21 22:53:07 +0500

    first thought. map[string][]string

--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+import "fmt"
+import "os"
+// flags: Number Of Words(-w); Prefix(-p); Prefix Length(-l);
+
+
 func main() {
+	text, err := os.ReadFile("temptext.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	words := []string {}
+	reader := ""
+	for _, char := range text {
+		if char == '\n' || char == ' ' {
+			if len(reader) != 0 {
+				words = append(words, reader)
+				reader = ""
+			}
+			continue
+		}
+		reader += string(char)
+	}
+	if len(reader) != 0 {
+		words = append(words, reader)
+	}
+	
+	WordsMap := map[string][]string {}
+
+	for i := 0; i < len(words)-2; i++ {
+		WordsMap[words[i]+" "+words[i+1]] = append(WordsMap[words[i]+" "+words[i+1]], words[i+2])
+	}
+
+	fmt.Println(WordsMap)
 }
---
+++ pkg/io/help.go
@@ -0,0 +1,15 @@
+package io
+
+import "fmt"
+
+func PrintHelp() {
+	fmt.Println("Markov Chain text generator.\n")
+	fmt.Println("Usage:"
+  	fmt.Println("markovchain [-w <N>] [-p <S>] [-l <N>]")
+  	fmt.Println("markovchain --help\n")
+	fmt.Println("Options:")
+	fmt.Println("  --help  Show this screen.")
+	fmt.Println("  -w N    Number of maximum words")
+	fmt.Println("  -p S    Starting prefix")
+	fmt.Println("  -l N    Prefix length")
+}
