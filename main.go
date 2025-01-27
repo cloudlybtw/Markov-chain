87c69e9266c3949041aabd7ff3c5b89333c3803b
dakabirov <cloudlypower@gmail.com>
2025-01-27 17:34:42 +0500

    queue + converting to string for key

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	// "fmt"
-	// "os"
 	"flag"
 	"fmt"
 	"os"
+	"io"
 )
 
 // flags: Number Of Words(-w); Prefix(-p); Prefix Length(-l);
@@ -44,15 +43,18 @@ func (q *Queue) GetString() string {
 	return str
 }
 
+func (q *Queue) Len() int {
+	return len(q.queue)
+}
+
+
 var (
 	numWordsFlag  = flag.Int("w", 100, "help him")
-	prefixLenFlag = flag.Int("l", 2, "nah dont help him")
+	lenFlag = flag.Int("l", 2, "nah dont help him")
+	prefixFlag = flag.String("p", "", "never help anyone")
 )
 
 func main() {
-	// var numWordsFlag = flag.Int("w", 100, "help him")
-	// // var prefixFlag = flag.String("")
-	// var prefixLenFlag = flag.Int("l", 2, "nah dont help him")
 	flag.Parse()
 	if *numWordsFlag < 0 {
 		fmt.Fprintln(os.Stderr, "The maximum number of words can't be negative.")
@@ -61,42 +63,51 @@ func main() {
 		fmt.Fprintln(os.Stderr, "The maximum number of words can't be more than 10,000.")
 		os.Exit(1)
 	}
-	if *prefixLenFlag < 1 {
+	if *lenFlag < 1 {
 		fmt.Fprintln(os.Stderr, "The prefix length can't be less than 1.")
 		os.Exit(1)
-	} else if *prefixLenFlag > 5 {
+	} else if *lenFlag > 5 {
 		fmt.Fprintln(os.Stderr, "The prefix length can't be more than 5.")
 		os.Exit(1)
 	}
 	fmt.Println(*numWordsFlag)
-	fmt.Println(*prefixLenFlag)
+	fmt.Println(*lenFlag)
+	text, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error while reading input.")
+		os.Exit(1)
+	}
 
 	// text, err := os.ReadFile("temptext.txt")
 	// if err != nil {
 	// 	os.Exit(1)
 	// }
-	// words := []string{}
-	// reader := ""
-	// for _, char := range text {
-	// 	if char == '\n' || char == ' ' {
-	// 		if len(reader) != 0 {
-	// 			words = append(words, reader)
-	// 			reader = ""
-	// 		}
-	// 		continue
-	// 	}
-	// 	reader += string(char)
-	// }
-	// if len(reader) != 0 {
-	// 	words = append(words, reader)
-	// }
-
-	// WordsMap := map[string][]string{}
-
-	// queue := NewQueue()
-	// for i := 0; i < 5; i++ { //len(words)-2
-	// 	queue.Push(words[i])
-	// }
-	// fmt.Println(queue.GetString())
-	// fmt.Println(WordsMap)
+	WordsMap := map[string][]string{}
+	readingQueue := NewQueue()
+	reader := ""
+	for _, char := range text {
+		if char == '\n' || char == ' ' {
+			if len(reader) != 0 {
+				if readingQueue.Len() != *lenFlag {
+					readingQueue.Push(reader)
+					reader = ""
+					continue
+				}
+				WordsMap[readingQueue.GetString()] = append(WordsMap[readingQueue.GetString()], reader)
+				_ = readingQueue.Pop()
+				readingQueue.Push(reader)
+				reader = ""
+			}
+			continue
+		}
+		reader += string(char)
+	}
+	if len(reader) != 0 {
+		WordsMap[readingQueue.GetString()] = append(WordsMap[readingQueue.GetString()], reader) //" (end)"
+		_ = readingQueue.Pop()
+		readingQueue.Push(reader)
+	}
+	WordsMap[readingQueue.GetString()] = append(WordsMap[readingQueue.GetString()], " (end)")
+	
+	fmt.Println(WordsMap)
 }
